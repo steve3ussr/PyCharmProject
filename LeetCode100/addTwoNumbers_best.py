d82bc00ed237b088057faaccc789eb938ce19302
Steve3ussr <2816825048@qq.com>
2022-03-15 18:50:35 +0800

    0315C

--- LeetCode100/addTwoNumbers_best.py
+++ LeetCode100/addTwoNumbers_best.py
@@ -1,22 +1,23 @@
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(0)
+        ans = head
+        flag = 0
 
-        result = ListNode(0)
-        r = result
-        carry = 0
-        while (l1 or l2):
+        while l1 or l2:
             x = l1.val if l1 else 0
             y = l2.val if l2 else 0
-            local_sum = x + y + carry
-            carry = local_sum // 10
-            r.next = ListNode(local_sum % 10)
-            r = r.next
-            if l1 != None:
-                l1 = l1.next
 
-            if l2 != None:
+            sum_ = flag + x + y
+            ans.next = ListNode(sum_ % 10)
+            flag = sum_ // 10
+            ans = ans.next
+
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
                 l2 = l2.next
 
-        if carry > 0:
-            r.next = ListNode(1)
-        return result.next
+        if flag > 0:
+            ans.next = ListNode(1)
+        return head.next
