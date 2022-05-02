f3679442db51696958c7e495585d278004ba7080
Steve3ussr <2816825048@qq.com>
2022-05-02 23:31:30 +0800

    0502C

--- DSA_python/pythonds/trees/AVL_BST.py
+++ DSA_python/pythonds/trees/AVL_BST.py
@@ -38,14 +38,50 @@ class AVL_BST(BinarySearchTree):
             self.updateBalance(node.parent)
 
     def rebalance(self, node):
-        # TODO: unkonwn
+        # TODO: unknown
         pass
 
-    def rotateLeft(self, node):
-        pass
+    def rotateLeft(self, rotRoot: TreeNode):
+        # 1. root.parent connection
+        newRoot = rotRoot.rightChild
+        newRoot.parent = rotRoot.parent
+        if rotRoot.parent:
+            if rotRoot.isLeftChild():
+                rotRoot.parent.leftChild = newRoot
+            else:
+                rotRoot.parent.rightChild = newRoot
+        else:
+            self.root = newRoot
+        # 2. rotRoot.rightChild rebuild
+        rotRoot.rightChild = newRoot.leftChild
+        if newRoot.leftChild:
+            newRoot.leftChild.parent = rotRoot
+        # 3. rotRoot.parent rebuild
+        newRoot.leftChild = rotRoot
+        rotRoot.parent = newRoot
+        # 4. parent nodes balanceFactor recalculate
+        rotRoot.balanceFactor += 1 - min(newRoot.balanceFactor, 0)
+        newRoot.balanceFactor += 1 + max(rotRoot.balanceFactor, 0)
 
-    def rotateRight(self, node):
-        pass
+    def rotateRight(self, rotRoot: TreeNode):
+        newRoot = rotRoot.leftChild
+        newRoot.parent = rotRoot.parent
+        if rotRoot.parent:
+            if rotRoot.isLeftChild():
+                newRoot.parent.leftChild = newRoot
+            else:
+                newRoot.parent.rightChild = newRoot
+        else:
+            self.root = newRoot
 
+        rotRoot.leftChild = newRoot.rightChild
+        if rotRoot.leftChild:
+            rotRoot.leftChild.parent = rotRoot
+
+        newRoot.rightChild = rotRoot
+        rotRoot.parent = newRoot
+
+        rotRoot.balanceFactor += -1 - max(0, newRoot.balanceFactor)
+        newRoot.balanceFactor += -1 + min(0, rotRoot.balanceFactor)
 
 
