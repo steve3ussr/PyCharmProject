e8980ff4ca44b57845409f9531d843c7bbf1eabf
Steve3ussr <2816825048@qq.com>
2022-06-28 10:56:14 +0800

    220628A

--- LeetCode100/generateParenthesis.py
+++ LeetCode100/generateParenthesis.py
@@ -1,2 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
+        if n == 1:
+            return ['()']
+        else:
+            res_last = self.generateParenthesis(n-1)
+            res = []
+            for item in res_last:
+                res.append('(' + item + ')')
+                res.append('()' + item)
+                res.append(item + '()')
+
+        res.remove('()' * n)
+        return res
+
+
+if __name__ == '__main__':
+    res = Solution().generateParenthesis(10)
+    print(f'{len(res)}: {res}')
+
+, '()((()))', '()(()())',                            '()(())()', '()(())()'
+
+haodhaa , '()((()))', '()(()())',
