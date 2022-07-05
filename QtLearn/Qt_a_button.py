84bad7befd042827b86a847e017761a796a363c8
Steve3ussr <2816825048@qq.com>
2022-07-05 21:18:21 +0800

    220705C: PySide6, Qt

---
+++ QtLearn/Qt_a_button.py
@@ -0,0 +1,16 @@
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtCore import Slot
+
+
+@Slot()
+def say_hello():
+    print("Button clicked, Hello!")
+
+
+app = QApplication(sys.argv)
+button = QPushButton('DO NOT PRESS F')
+
+button.clicked.connect(say_hello)
+button.show()
+app.exec()
---
+++ QtLearn/Qt_a_dialog.py
@@ -0,0 +1,31 @@
+import sys
+
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QApplication, QPushButton, QDialog, QVBoxLayout, QLineEdit
+
+
+class Adialog(QDialog):
+    def __init__(self, parent=None):
+        super(Adialog, self).__init__(parent)
+        self.button = QPushButton('goto love')
+        self.edit_zone = QLineEdit('the lucky one')
+        self.setWindowTitle('loving U')
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+        layout.addWidget(self.edit_zone)
+        self.setLayout(layout)
+        self.button.clicked.connect(self.loveing)
+
+    @Slot()
+    def loveing(self):
+        print(f'Go and love yourself, {self.edit_zone.text()}')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    a_dialog = Adialog()
+    a_dialog.show()
+    sys.exit(app.exec())
+    # app.exec()
+    
