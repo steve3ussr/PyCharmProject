ee32b51eb94701ec207d929f244709718493837e
steve3ussr <2816825048@qq.com>
2022-07-12 09:15:11 +0800

    220711C

--- QtLearn/load_file/load_file_main.py
+++ QtLearn/load_file/load_file_main.py
@@ -13,9 +13,19 @@ class MainWindow(QMainWindow):
         self.setter_file(self.ui.toolButton_src_hex_s19, self.ui.lineEdit_src_hex_s19, '*.hex; *.s19')
         self.setter_file(self.ui.toolButton_tgt_a2l, self.ui.lineEdit_tgt_a2l, '*.a2l')
         self.setter_file(self.ui.toolButton_tgt_hex_s19, self.ui.lineEdit_tgt_hex_s19, '*.hex; *.s19')
+
         self.setter_file(self.ui.toolButton_dcm, self.ui.lineEdit_dcm, '*.dcm')
-        self.setter_folder(self.ui.toolButton_xlsx, self.ui.lineEdit_xlsx)
-        self.setter_folder(self.ui.toolButton_merged, self.ui.lineEdit_merged)
+        self.setter_folder(self.ui.toolButton_xlsx, self.ui.lineEdit_xlsx, '*.xlsx')
+        self.setter_folder(self.ui.toolButton_merged, self.ui.lineEdit_merged, '*.xlsx')
+
+
+    """def res_format(self):
+        if self.ui.radioButton_hex.isChecked():
+            return '*.hex'
+        elif self.ui.radioButton_s19.isChecked():
+            return '*.s19'
+        else:
+            raise"""
 
     def setter_file(self, a_button, a_line, a_str):
         a_button.clicked.connect(self.select_file(a_line, a_str))
@@ -27,22 +37,21 @@ class MainWindow(QMainWindow):
                                                     caption="选择文件",
                                                     dir=".",
                                                     filter=a_str)[0]  # 选择目录，返回选中的路径
-            # print(type(file_path))
             a_line.setText(file_path)
         return select_file_inner
 
-    def setter_folder(self, a_button, a_line):
-        a_button.clicked.connect(self.select_folder(a_line))
+    def setter_folder(self, a_button, a_line, a_str):
+        a_button.clicked.connect(self.select_folder(a_line, a_str))
 
     @Slot()
-    def select_folder(self, a_line):
+    def select_folder(self, a_line, a_str):
         def select_folder_inner():
-            folder_path = QFileDialog.getExistingDirectory(QMainWindow(), "选择文件夹")  # 选择目录，返回选中的路径
-            # print(type(file_path))
+            folder_path = QFileDialog.getSaveFileName(QMainWindow(),
+                                                      caption="保存文件",
+                                                      dir=".",
+                                                      filter=a_str)[0]  # 选择目录，返回选中的路径
             a_line.setText(folder_path)
-
         return select_folder_inner
-
 
 
 if __name__ == '__main__':
