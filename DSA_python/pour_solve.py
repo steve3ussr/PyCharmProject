e1c1a5ddf3e44f13993f7d14501afcc16a9e4a2d
steve3ussr <2816825048@qq.com>
2022-07-27 23:55:57 +0800

    220727C: pour fix

--- DSA_python/pour_solve.py
+++ DSA_python/pour_solve.py
@@ -1,74 +1,33 @@
-import pprint
-import sys
+from pprint import pprint as pp
 
 
 class Solution(object):
-    def __init__(self, large: int, small: int, expected: int):
-        self.path1 = []
-        self.path2 = []
-        self.l_limit = large
-        self.s_limit = small
-        self.e = expected
-        self.l = 0
-        self.s = 0
-        self.init_l2s()
-        self.init_s2l()
+    def __init__(self, x, y, e):
+        self.__x = x
+        self.__y = y
+        self.__e = e
+        self.__path_l2s = []
+        self.__path_s2l = []
 
-
-    def l2s(self):
-        s_rest = self.s_limit - self.s
-        if s_rest == 0:
-            self.s = 0
-
-        elif self.l == 0:
-            self.l += self.l_limit
-
-        elif s_rest > self.l:
-            self.s += self.l
-            self.l = 0
-
+    def __l2s(self, i):
+        self.__path_l2s.append(i)
+        if i == self.__e:
+            print('l2s Done')
         else:
-            self.s += s_rest
-            self.l -= s_rest
+            return self.__l2s(i - self.__y) if i > self.__y else self.__l2s(i + self.__x - self.__y)
 
-        self.path1.append((self.l, self.s))
-
-    def s2l(self):
-        l_rest = self.l_limit - self.l
-        if l_rest == 0:
-            self.l = 0
-
-        elif self.s == 0:
-            self.s += self.s_limit
-
-        elif l_rest > self.s:
-            self.l += self.s
-            self.s = 0
-
+    def __s2l(self, i):
+        self.__path_s2l.append(i)
+        if i == self.__e:
+            print("s2l Done")
         else:
-            self.l += l_rest
-            self.s -= l_rest
+            return self.__s2l(tmp) if (tmp := i + self.__y) < self.__x else self.__s2l(tmp - self.__x)
 
-        self.path2.append((self.l, self.s))
-
-    def init_l2s(self):
-        self.l = self.l_limit
-        self.s = 0
-        self.path1.append((self.l, self.s))
-        while self.l != self.e:
-            self.l2s()
-
-    def init_s2l(self):
-        self.s = self.s_limit
-        self.l = 0
-        self.path2.append((self.l, self.s))
-        while self.l != self.e:
-            self.s2l()
-
-    def exec(self):
-        return self.path1 if len(self.path1) <= len(self.path2) else self.path2
+    def main(self):
+        self.__l2s(self.__x)
+        self.__s2l(0)
+        pp(self.__path_l2s) if len(self.__path_l2s) < len(self.__path_s2l) else pp(self.__path_s2l)
 
 
 if __name__ == '__main__':
-    inst = Solution(4, 3, 2)
-    pprint.pprint(inst.exec())
+    Solution(11, 4, 5).main()
