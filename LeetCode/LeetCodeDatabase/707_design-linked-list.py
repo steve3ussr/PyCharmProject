7e3e1435399ba5c7d13671d0c84cca5bcd3024a6
steve3ussr <2816825048@qq.com>
2023-06-14 04:06:44 +0800

    rebuild LC-707

--- LeetCode/LeetCodeDatabase/707_design-linked-list.py
+++ LeetCode/LeetCodeDatabase/707_design-linked-list.py
@@ -15,46 +15,49 @@ class MyLinkedList:
     def __len__(self):
         return self._len
 
-    def __str__(self):
-        if self._len == 0:
-            return f"{self._len}, {[]}"
+    def __getitem__(self, i):
+        return self.get[i]
 
-        curr = self.head
+    def __str__(self):
         res = []
-        while curr:
-            res.append(curr.val)
-            curr = curr.next
-        return f"{self._len}, {res}"
+        if self._len:
+            curr = self.head
+            while curr:
+                res.append(curr.val)
+        return str(res)
 
     __repr__ = __str__
 
-    def __getitem__(self, index):
-        if not self._len:  # empty
+    def _get_node(self, index: int) -> ListNode | None:
+        if self._len == 0:
             return None
-        elif self._len == 1:
-            return self.head
+
+        if index >= self._len or index < 0:
+            return None
 
         if index <= self._len // 2:
-            curr = self.head
             cnt = 0
+            curr = self.head
             while cnt < index:
                 curr = curr.next
                 cnt += 1
             return curr
         else:
-            cnt = self._len-1
+            cnt = self._len - 1
             curr = self.tail
-
             while cnt > index:
                 curr = curr.prev
                 cnt -= 1
             return curr
 
     def get(self, index: int) -> int:
-        return self[index].val if 0<=index<self._len else -1
+        if _ := self._get_node(index):
+            return _.val
+        else:
+            return -1
 
     def addAtHead(self, val: int) -> None:
-        if self.head is None:
+        if not self._len:
             self.head = self.tail = ListNode(None, val, None)
         else:
             self.head.prev = ListNode(None, val, self.head)
@@ -63,81 +66,50 @@ class MyLinkedList:
         self._len += 1
 
     def addAtTail(self, val: int) -> None:
-        if self.head is None:
+        if not self._len:
             self.head = self.tail = ListNode(None, val, None)
         else:
             self.tail.next = ListNode(self.tail, val, None)
             self.tail = self.tail.next
+
         self._len += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if self._len == 0:
-            if index == 0:
-                self.addAtHead(val)
-            return
-
-        if index < 0 or index > self._len:
-            return
-
         if index == self._len:
-            self.addAtTail(val)
-            return
+            return self.addAtTail(val)
 
         if index == 0:
-            self.addAtHead(val)
-            return
+            return self.addAtHead(val)
 
-        target = self[index]
-        target.prev.next = ListNode(target.prev, val, target)
-        target.prev = target.prev.next
-        self._len += 1
+        if target := self._get_node(index):
+            target.prev.next = ListNode(target.prev, val, target)
+            target.prev = target.prev.next
+            self._len += 1
 
     def deleteAtIndex(self, index: int) -> None:
-        if index >= self._len:
-            return
-        elif index < 0:
-            return
-        elif self._len == 0:
-            return
+        if target := self._get_node(index):
 
-        if index == 0:
-            if self._len == 1:
-                self.tail = self.head = None
+            if index == 0:
+                if self.head.next:
+                    self.head = self.head.next
+                    self.head.prev = None
+                else:
+                    self.head = self.tail = None
+            elif index == self._len - 1:
+                self.tail = self.tail.prev
+                self.tail.next = None
             else:
-                self.head = self.head.next
-                self.head.prev = None
+                target.prev.next = target.next
+                target.next.prev = target.prev
+
             self._len -= 1
-            return
-
-        if index == self._len-1:
-            self.tail = self.tail.prev
-            self.tail.next = None
-            self._len -= 1
-            return
-
-        target = self[index]
-        target.prev.next = target.next
-        target.next.prev = target.prev
-        self._len -= 1
-
-    def extend(self, seq):
-        if len(seq) < 1:
-            return
-
-        for _ in seq:
-            self.addAtTail(_)
 
 
 if __name__ == '__main__':
     inst = MyLinkedList()
-    inst.addAtHead(7); print(inst)
-    inst.addAtTail(7); print(inst)
-    inst.addAtHead(9); print(inst)
-    inst.addAtTail(8); print(inst)
-    inst.addAtHead(6); print(inst)
-    inst.addAtHead(0); print(inst)
+    inst.addAtIndex(2, 1); print(inst)
+    inst.addAtIndex(3, 4);
+    print(inst)
+    inst.addAtTail(1); print(inst)
 
-    print(inst[5].val)
-    inst.addAtHead(0); print(inst)
-    print(inst[2].val)
-    print(inst[5].val)
+
